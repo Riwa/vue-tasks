9d52c6458a2a136b75fc12facbb93a10fce6a34c
Arthur Alloncle <arthuralloncle@gmail.com>
2017-05-13 16:41:10 +0200

    Creating base files

---
+++ src/Store.js
@@ -0,0 +1,66 @@
+export const Store = {
+	datas: {
+		tasks: [
+			{
+				title: 'Créer le formulaire de contact',
+				criticality: 3,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Valider la maquette',
+				criticality: 8,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Acheter de l\'uranium bio',
+				criticality: 6,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Régler l\'issue #44',
+				criticality: 9,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Corriger les liens morts',
+				criticality: 5,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Valider le compte rendu',
+				criticality: 2,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Remplacer les pneus du caniche',
+				criticality: 1,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Supprimer les branches inutiles sur GitHub',
+				criticality: 6,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Eplucher le hamster',
+				criticality: 9,
+				hours: 5,
+				visibility: true,
+			},
+			{
+				title: 'Rédiger les mentions légales',
+				criticality: 1,
+				hours: 5,
+				visibility: true,
+			}
+		]
+	}
+}
