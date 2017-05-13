e9ef435a29c0f6356006723de8c70afc13f5ddc1
Arthur Alloncle <arthuralloncle@gmail.com>
2017-05-13 17:15:31 +0200

    Displaying tasks in List component

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+	<ul class="collection with-header">
+		<li class="collection-header"><h4>Tâches en attente</h4></li>
+		<task v-for="task in tasks" :tsk="task"></task>
+	</ul>
+</template>
+
+<script>
+import {Store} from '@/Store.js'
+import Task from '@/components/Task'
+
+export default {
+	components: {
+		task: Task,
+	},
+	data() {
+		return {
+			tasks: Store.datas.tasks,
+		}
+	}
+}
+
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+	<li class="collection-item"><div>{{ tsk.title }}<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
+</template>
+
+<script>
+	import {Store} from '@/Store.js'
+
+	export default {
+		name: 'task',
+		props: ['tsk'],
+		data(){
+			return {
+				tasks: Store.datas.tasks
+			}
+		}
+	}
+</script>
